af5d6cdfcff8fdb685321bf6441672a7a2f573e9
bajaj-hitesh <hitesh.smit@gmail.com>
2024-10-26 21:32:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var uuid = require('uuid')
-const { createUser, getUser, updateUser, deleteUser, auth } = require('./utils');
+const applicationRouter = require('./routes');
+const { auth } = require('./utils');
 var db = require('./db');
 
 var app = express();
@@ -10,64 +11,13 @@ app.use(express.json({
     type: ['application/json', 'application/scim+json']
 }));
 
+app.use('/', require('./routes'));
 
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-// SCIM endpoint for creating a user
-app.post('/scim/v2/users', (req, res) => {
-
-    let user = req.body;
-    const { userName } = user;
-    const id = uuid.v4();
-    user.id = id;
-
-    console.log(`User Object being saved: ${JSON.stringify(user)}`)
-    const profileStr = JSON.stringify(user);
-
-    db.run(`INSERT INTO users (id, userName, json_body) VALUES (?, ?, ?)`,
-        [id, userName, profileStr],
-        function (err) {
-            if (err) {
-                return res.status(400).json({ detail: "User creation failed", status: 400 });
-            }
-            res.status(201).json(user);
-        }
-    );
-});
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
-
-
-// SCIM endpoint for retrieving only the JSON field for all users
-app.get('/scim/v2/users', (req, res) => {
-
-    const startIndex = parseInt(req.query.startIndex) || 1; // SCIM 1-based index
-    const count = parseInt(req.query.count) || 100;          // Default to 10 users per page
-
-    // Calculate the offset for SQL (SQLite uses 0-based index)
-    const offset = startIndex - 1;
-
-
-    db.all(`SELECT id,json_body FROM users LIMIT ? OFFSET ?`,[count, offset], (err, rows) => {
-        if (err) {
-            return res.status(500).json({ detail: "Error retrieving users", status: 500 });
-        }
-
-
-        // Format response as per SCIM v2.0 list response with only the JSON field
-        const scimResponse = {
-            schemas: ["urn:ietf:params:scim:api:messages:2.0:ListResponse"],
-            totalResults: rows.length,
-            Resources: rows.map(user => (JSON.parse(user.json_body)))};
-
-        res.json(scimResponse);
-    });
-});
 
 // SCIM endpoint for deleting a user by ID
 app.delete('/scim/v2/Users/:id', (req, res) => {
